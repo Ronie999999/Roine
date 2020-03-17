7636d719d0b3805116452ec3362713bafb56ecd1
e <1033190331@stu.jiangnan.edu.cn>
2020-03-17 23:03:33 +0800

    QR code

---
+++ python/myqr/myqr_dynamic.py
@@ -0,0 +1,6 @@
+import MyQR.myqr
+MyQR.myqr.run(words="https://github.com/Ronie999999",
+              version=8,
+              picture="tai_chi.gif",
+              save_name="taichi.gif",
+              colorized=True)
---
+++ python/myqr/myqr_static .py
@@ -0,0 +1,6 @@
+import MyQR.myqr
+MyQR.myqr.run(words="https://github.com/Ronie999999",
+              version=8,
+              picture="hacker.jpg",
+              save_name="hacker.png",
+              colorized=True)
