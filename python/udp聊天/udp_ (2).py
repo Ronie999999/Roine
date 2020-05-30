6d09d2f6266599df9cb54cc05581db420a13bb41
e <1033190331@stu.jiangnan.edu.cn>
2020-05-30 16:56:11 +0800

    udp

---
+++ python/udp聊天/udp_ (2).py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import sys
+
+
+def send(s):
+    while True:
+        data = input()
+        if data == 'exit':
+            sys.exit(0)
+        s.sendto(data.encode('utf-8'), ('127.0.0.1', 8080))
+
+
+def recv_(s):
+    s.bind(("127.0.0.1", 8090))
+    while True:
+        data = s.recvfrom(1024)
+        print("recive from " + str(data[1]) + "   data: " + data[0].decode())
+
+
+def main():
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = threading.Thread(target=send, args=(udp,))
+    r = threading.Thread(target=recv_, args=(udp,))
+    s.start()
+    r.start()
+    r.join()
+    s.join()
+    udp.close()
+
+
+if __name__ == "__main__":
+    main()
